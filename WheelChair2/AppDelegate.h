53607b7ff6a949359cef247923a17bdf29d72657
vitorcarmovieira <vitorcarmovieira@gmail.com>
2015-06-08 16:53:17 -0400

    First

---
+++ WheelChair2/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  WheelChair2
+//
+//  Created by Vitor on 6/8/15.
+//  Copyright (c) 2015 Bepid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ WheelChair2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WheelChair2
+//
+//  Created by Vitor on 6/8/15.
+//  Copyright (c) 2015 Bepid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
